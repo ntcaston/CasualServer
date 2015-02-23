f2538d12d1a42c042dbf44a341c039e19a748386
Nick Caston <ntcaston@gmail.com>
2015-02-23 23:21:15 +1100

    allow multivalue response headers to be set in separate calls

--- src/spikedog/casual/server/Response.java
+++ src/spikedog/casual/server/Response.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Basic HTTP response representation.
@@ -18,8 +23,9 @@ public final class Response {
 
   private final OutputStream out;
 
-  // TODO sorted set?.. map?
-  private final List<String> headers = new ArrayList<String>();
+  private final LinkedHashMap<String, List<String>> headers =
+      new LinkedHashMap<String, List<String>>();
+  private final Set<String> writtenHeaders = new HashSet<String>();
   private StatusLine statusLine;
   private boolean statusLineWritten = false;
   private boolean begunWritingBody = false;
@@ -31,16 +37,39 @@ public final class Response {
   }
 
   public void setStatusLine(StatusLine statusLine) {
+    if (statusLineWritten) {
+      throw new IllegalStateException("Setting status line after status line written to output.");
+    }
     this.statusLine = statusLine;
   }
 
   public void addHeader(String name, String value) {
-    // TODO going to have to actually parse these to know when to close.... maybe?
-    // TODO deal with multi-value
-    headers.add(name + ": " + value);
+    if (writtenHeaders.contains(name)) {
+      throw new IllegalStateException(
+          "Attempted to change a header which has already been written to output.");
+    }
+    if (!headers.containsKey(name)) {
+      headers.put(name, new ArrayList<String>(1));
+    }
+    List<String> valueList = headers.get(name);
+    valueList.add(value);
+  }
+
+  public void setHeader(String name, String value) {
+    if (writtenHeaders.contains(name)) {
+      throw new IllegalStateException(
+          "Attempted to set a header which has already been written to output.");
+    }
+    List<String> values = new ArrayList<String>(1);
+    values.add(value);
+    headers.put(name, values);
   }
 
   public void setBody(InputStream body) {
+    if (begunWritingBody) {
+      throw new IllegalStateException(
+          "Attempted to change message body after begun writing body to output.");
+    }
     this.body = body;
   }
 
@@ -50,17 +79,33 @@ public final class Response {
     if (!statusLineWritten && statusLine != null) {
       out.write(statusLine.toString().getBytes());
       out.write(NEW_LINE_BYTES);
+      statusLineWritten = true;
     }
 
     if (headers.size() > writtenHeaderCount && !begunWritingBody) {
-      for (int headerIndex = writtenHeaderCount; headerIndex < headers.size(); headerIndex++) {
-        out.write(headers.get(headerIndex).getBytes());
+      Iterator<Map.Entry<String, List<String>>> iterator = headers.entrySet().iterator();
+      for (int headerIndex = 0; headerIndex < headers.size(); headerIndex++) {
+        Map.Entry<String, List<String>> entry = iterator.next();
+        if (headerIndex < writtenHeaderCount) {
+          continue;
+        }
+
+        String name = entry.getKey();
+        List<String> values = entry.getValue();
+        StringBuilder valueStringBuilder = new StringBuilder();
+        for (int valueIndex = 0; valueIndex < values.size(); valueIndex++) {
+          valueStringBuilder.append(values.get(valueIndex));
+          if (valueIndex < values.size() - 1) {
+            valueStringBuilder.append(",");
+          }
+        }
+        out.write((name + ": " + valueStringBuilder.toString()).getBytes());
         out.write(NEW_LINE_BYTES);
       }
       writtenHeaderCount = headers.size();
     }
 
-    if (body != null) {
+    if (body != null && !begunWritingBody) {
       begunWritingBody = true;
       out.write(NEW_LINE_BYTES);
       int n = 0;
